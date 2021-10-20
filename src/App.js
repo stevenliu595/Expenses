e40c6148b565e6afb7ae247ca91454fa0b167b55
stevenliu595 <sliu57@buffalo.edu>
2021-10-19 20:28:39 -0400

    tweaks

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ const initialExpenses = [
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: "Nintendo Switch",
+    amount: 299.99,
+    date: new Date(2021, 10, 15),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
+    title: "New Standing Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
